70819c68b3f1c29fa76dd3c356614765ef542c76
MariiaAntoniuk <mariia.antonik18@gmail.com>
2023-05-04 15:04:31 +0300

    fix styles

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ let login = document.querySelector(".login-form")
 
 document.querySelector("#login-btn").onclick = () => {
       login.classList.toggle('active');
+      navbar.classList.remove('active')
 }
 
 let navbar = document.querySelector(".header .navbar");
 
-document.querySelector('#menu-btn').onclick = ()  => {
+document.querySelector("#menu-btn").onclick = ()  => {
     login.classList.remove('active');
     navbar.classList.toggle('active');
 }
